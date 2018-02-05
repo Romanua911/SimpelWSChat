bc5563d190da10f9ad8969c300c37bea4b461507
romanua911 <kuc911@gmail.com>
2018-02-05 16:47:35 +0200

    Study

---
+++ server.js
@@ -0,0 +1,25 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({ port: 3000 });
+
+let masmessage = '';
+
+server.on('connection', ws => {
+
+    ws.on('message', message => {
+        if (message === 'exit') {
+            ws.close();
+        } else if(message === '-arhive'){
+            ws.send(masmessage);
+        }else {
+            server.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(message);
+                    masmessage=masmessage+'<br>'+message;
+                }
+            });
+        }
+    });
+
+    ws.send('Добро пожаловать в CodeDojo. \n Архив -arhive');
+});
